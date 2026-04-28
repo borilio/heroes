989bee3dd5e50f7a1f3daae2da3df252cfe99efd
borilio <exprimetuandroid@gmail.com>
2026-04-28 10:56:14 +0200

    Fix: Eliminado componentes extra que estaban importados innecesariamente y provocaban warning que podían despistar

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/layout/header/header";
 import { Navbar } from "./components/layout/navbar/navbar";
-import { Home } from "./components/pages/home/home";
 import { Footer } from "./components/layout/footer/footer";
-import { HeroesList } from "./components/pages/heroes-list/heroes-list";
-import { HeroesManage } from "./components/pages/heroes-manage/heroes-manage";
-import { About } from "./components/pages/about/about";
 
 @Component({
   selector: 'app-root',
@@ -14,11 +10,7 @@ import { About } from "./components/pages/about/about";
     RouterOutlet,
     Header,
     Navbar,
-    Home,
-    Footer,
-    HeroesList,
-    HeroesManage,
-    About
+    Footer
 ],
   templateUrl: './app.html',
   styleUrl: './app.css'
