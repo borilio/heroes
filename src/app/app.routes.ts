e141b044ee83e66a2b2d44f029db4fbc8fcf20ba
borilio <exprimetuandroid@gmail.com>
2026-04-28 10:18:59 +0200

    Terminado el v2-routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/pages/home/home';
+import { HeroesList } from './components/pages/heroes-list/heroes-list';
+import { HeroesManage } from './components/pages/heroes-manage/heroes-manage';
+import { About } from './components/pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: Home,
+    },
+    {
+        path: 'list',
+        component: HeroesList,
+    },
+    {
+        path: 'manage',
+        component: HeroesManage,
+    },
+    {
+        path: 'about',
+        component: About,
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+    },
+];
